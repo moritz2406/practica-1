82f75e819be9cfd28d8b6a7b8682eefd8a9b4766
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-04-13 14:40:02 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onSound(DetectedSound.Loud, function () {
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
+basic.showString("“Iniciando sistema de control de sonido y temperatura”")
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
 basic.forever(function () {
 	
 })
